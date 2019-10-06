01a543d7ec5161382302ba300de346b8026d7889
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2019-10-06 17:59:42 +0300

    More on french deck

--- Chapter_1/french_deck.py
+++ Chapter_1/french_deck.py
@@ -2,6 +2,7 @@ import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
+
 class FrenchDeck:
 
     ranks = [str(n) for n in range(2, 11)] + list('KQJA')
@@ -16,3 +17,26 @@ class FrenchDeck:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    print(card)
+    print(rank_value)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+if __name__ == '__main__':
+
+    beer_card =  Card('A', 'spades')
+    print(beer_card)
+
+    deck = FrenchDeck()
+    print(len(deck))
+    print(deck[-1])
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
